ca4433b8d2b74c0f19f5984924cf6d167e1efed8
leVert <selmamathis15@gmail.com>
2025-01-24 12:26:43 +0300

    updated fetchSession

--- lib/fetchSession.ts
+++ lib/fetchSession.ts
@@ -4,7 +4,7 @@ export async function getCurrentUser(req?: any) {
   const url = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/auth/session';
   const options: RequestInit = {
     method: 'GET',
-    credentials: 'same-origin',
+    credentials: 'include',
   };
 
   if (req) {
