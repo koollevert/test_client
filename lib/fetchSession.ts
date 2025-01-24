5a0ea6d24beca787107c286b9dd062c1ffdf3927
leVert <selmamathis15@gmail.com>
2025-01-24 13:21:20 +0300

    updated getCurrentUser helper

--- lib/fetchSession.ts
+++ lib/fetchSession.ts
@@ -1,7 +1,7 @@
-'use server';
+
 
 export async function getCurrentUser(req?: any) {
-  const url = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/auth/session';
+  const url = 'http://tikitika.devapi/auth/session';
   const options: RequestInit = {
     method: 'GET',
     credentials: 'include',
