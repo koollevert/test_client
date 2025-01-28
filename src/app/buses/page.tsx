8fe5fda5aa3c804edd739816eeb64e1e85459200
leVert <selmamathis15@gmail.com>
2025-01-28 20:27:15 +0300

    working with images

--- src/app/buses/page.tsx
+++ src/app/buses/page.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
 
-export default function page() {
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
+
+export default function ImageCard() {
   return (
-    <div>page</div>
-  )
+    <Card className="w-80 overflow-hidden">
+      <img
+        src="../../public/IMG_20240205_174752_0.jpg"
+        alt="Example Image"
+        className="w-full h-48 object-cover"
+      />
+      <CardContent className="p-4">
+        <CardHeader className="p-0">
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>A short description or supporting text goes here.</CardDescription>
+        </CardHeader>
+      </CardContent>
+    </Card>
+  );
 }
