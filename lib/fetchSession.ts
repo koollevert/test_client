9f8636002cbf55aae01613f8d82a108171e0aab0
leVert <selmamathis15@gmail.com>
2025-01-28 16:02:51 +0300

    commit

--- lib/fetchSession.ts
+++ lib/fetchSession.ts
@@ -1,12 +1,11 @@
 export async function fetchSession(req?: any) {
   const res = await fetch('http://auth-srv:3000/api/auth/session', {
     method: 'GET',
-    credentials: 'include',
-    headers: req?.headers,
+    headers: req ? req.headers  : undefined,
   });
 
-  if (!res.ok) return null;
+  
 
-  const data = await res.json();
-  return data.session || null;
+  const {currentUser} = await res.json();
+  return currentUser || null;
 }
