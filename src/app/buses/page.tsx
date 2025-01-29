91d6c4b56de00d920075a1413522f546a06fab49
leVert <selmamathis15@gmail.com>
2025-01-29 11:50:15 +0300

    button test

--- src/app/buses/page.tsx
+++ src/app/buses/page.tsx
@@ -77,6 +77,7 @@ export default function PropertyCard() {
           </p>
         </div>
       </div>
+      <Button>Press me</Button>
     </div>
   );
 }
@@ -89,6 +90,5 @@ import {Button} from "@heroui/button";
 
 function App() {
   return (
-    <Button>Press me</Button>
   );
 }
